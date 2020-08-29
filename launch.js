845bc541b138125d6a8beb7d9442cef8efb3e9e3
Andrew Kong <50548798+DMNight6@users.noreply.github.com>
2020-08-29 09:31:11 +0800

    Create launch.js

---
+++ launch.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+const http = require('http')
+let application = fs.readFileSync('./application.yml', 'utf8')
+
+if (process.env.PORT) {
+    application = application.replace('PORT', process.env.PORT)
+}
+
+if (process.env.PASS) {
+    application = application.replace('PASS', process.env.PASS)
+}
+fs.writeFileSync('./application.yml', application)
+
+function startLavalink() {
+    const spawn = require('child_process').spawn;
+    const child = spawn('java', ['-jar', 'Lavalink.jar'])
+
+    child.stdout.setEncoding('utf8')
+    child.stderr.setEncoding('utf8')
+
+    child.stdout.on('data', (data) => {
+        console.log(data);
+    });
+
+    child.stderr.on('data', (data) => {
+        console.error(data);
+    });
+
+    child.on('error', (error) => {
+        console.error(error);
+    });
+
+    child.on('close', (code) => {
+        console.log(`Lavalink exited with code ${code}`);
+    });
+}
+
+const cdn = 'http://cdn.glitch.com/9cb86b78-568c-4263-8bbe-456709adf52f%2FLavalink.jar?v=1594771592451'
+download(cdn, './Lavalink.jar', startLavalink)
